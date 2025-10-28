0278f6564a7d236bb94824d327eabcee6ff91374
Alexandre Pinheiro <alexandre.ypinheiro@gmail.com>
2025-10-28 20:44:03 +1100

    creating models

---
+++ dbtcorenorthwind/models/staging/stg_order_details.sql
@@ -0,0 +1,19 @@
+-- models/staging/stg_order_details.sql
+
+with source as (
+    select * from {{ ref('raw_order_details')}}
+),
+
+filtered as (
+
+    select
+        order_id,
+        product_id,
+        unit_price,
+        quantity,
+        discount
+    from
+        source
+)
+
+select * from filtered
